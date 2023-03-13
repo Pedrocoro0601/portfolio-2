e7b8a3668787681c3837f4268a1fa43011e70f3e
Pedro Henrique <119066915+Pedrocoro0601@users.noreply.github.com>
2023-03-13 14:18:19 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+var formacao = document.getElementById("formacao")
+var iron = document.getElementById("iron")
+var panteranegra = document.getElementById("panteranegra")
+
+document.addEventListener('scroll', () => {   
+        iron.classList.add('homemdeferro')  
+        setInterval(()=>{
+            panteranegra.classList.add('panteranegra')  
+        }, 2000)
+        
+})
+
+
+var audio = new Audio('musica/spiderman');
